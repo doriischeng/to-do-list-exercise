7c0e98f7166d002cc66b1ba36d61ca97c738d978
doriischeng <dorisc712@gmail.com>
2022-12-08 12:40:46 +0800

    Modified the way of saving data in localStorage

--- app.js
+++ app.js
@@ -2,24 +2,21 @@ const form = document.querySelector("form");
 const input = document.querySelector("input");
 const list = document.querySelector("#todo-list");
 
-//check whether storage currently has saved items or is empty
-//if empty, create an array to store my todo list
-const savedTodos =
-  JSON.parse(localStorage.getItem("todos")) || [];
+document.addEventListener(
+  "DOMContentLoaded",
+  function (e) {
+    const savedTodos = JSON.parse(
+      localStorage.getItem("todos")
+    );
+    const location =
+      document.querySelector("#todo-list");
 
-//then to loop through the savedTodos array and display the result
-for (let i = 0; i < savedTodos.length; i++) {
-  let newTodo = document.createElement("li");
-  newTodo.innerText = savedTodos[i].task;
-  newTodo.isCompleted = savedTodos[i].isCompleted
-    ? true
-    : false;
-  if (newTodo.isCompleted) {
-    newTodo.style.textDecoration = "line-through";
+    location.insertAdjacentElement(
+      "afterbegin",
+      savedTodos
+    );
   }
-
-  list.appendChild(newTodo);
-}
+);
 
 //to add an item to the list
 form.addEventListener("submit", function (e) {
@@ -29,21 +26,17 @@ form.addEventListener("submit", function (e) {
     document.createElement("button");
   removeButton.innerText = "X";
   newTodo.innerText = input.value;
-  newTodo.isCompleted = false;
   newTodo.appendChild(removeButton);
-  input.value = "";
   list.appendChild(newTodo);
 
-  //to push newly added item into array and save it in storage
-  savedTodos.push({
-    task: newTodo.innerText,
-    isCompleted: false,
-  });
-  //to convert this entire JS array into string and save in storage
+  //save in localStorage
   localStorage.setItem(
     "todos",
-    JSON.stringify(savedTodos)
+    JSON.stringify(list.innerHTML)
   );
+
+  //reset the form after input
+  input.value = "";
 });
 
 //to remove an item from the list
@@ -55,17 +48,10 @@ list.addEventListener("click", function (e) {
   else if (e.target.tagName === "LI") {
     e.target.classList.add("completed");
   }
-  //to save above updates in storage
+  //to clear the localStorage and save the above updates in storage
+  localStorage.clear();
   localStorage.setItem(
     "todos",
-    JSON.stringify(savedTodos)
+    JSON.stringify(list.innerHTML)
   );
 });
-
-//Note for localStorage
-//need to save whenever you add, complete or remove
-//need to first make todo list into JS array
-//then can JSON encode the data to be stored
-//since localStorage only accepts key-value pair strings
-//then to get out, need to JSON decode it back to JS array
-//also need to display the result
